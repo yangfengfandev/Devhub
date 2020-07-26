98eae6a566a32fea05c8e524e9e61d960c114716
yangfengfan <yangfengfandev@gmail.com>
2020-07-26 18:42:21 +0800

    create project

---
+++ app/src/main/java/dev/yangfengfan/devhub/MainActivity.java
@@ -0,0 +1,17 @@
+package dev.yangfengfan.devhub;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * @author yangfengfandev@gmail.com 2020-07-26
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
